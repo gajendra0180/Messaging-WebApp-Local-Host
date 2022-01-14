7b51719e944655e8a6c5e0b779cd0a04ad8e9757
gajendra0180 <gajendrahitz@gmail.com>
2022-01-14 13:40:44 +0530

    Added Client UI

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,17 @@
 import React, { useRef } from "react";
 import { Container, Form, Button } from "react-bootstrap";
+import { v4 as uuidV4 } from "uuid";
 
-export default function Login() {
+export default function Login({ onIdSubmit }) {
   const idRef = useRef();
 
   function handleSubmit(e) {
     e.preventDefault();
+    console.log(idRef.current.value);
+    onIdSubmit(idRef.current.value);
+  }
+  function createNewId() {
+    onIdSubmit(uuidV4());
   }
 
   return (
@@ -13,15 +19,15 @@ export default function Login() {
       className="align-items-center d-flex"
       style={{ height: "100vh" }}
     >
-      <Form className="w-100">
+      <Form className="w-100" onSubmit={handleSubmit}>
         <Form.Group>
-          <Form className="label">
-            <Form className="label">Enter Your Id</Form>
-            <Form.Control type="text" ref={idRef}></Form.Control>
-          </Form>
+          <Form.Label className="label">Enter Your Id</Form.Label>
+          <Form.Control type="text" ref={idRef}></Form.Control>
         </Form.Group>
         <Button type="submit">Login</Button>
-        <Button variant="secondary">Create a new Id</Button>
+        <Button onClick={createNewId} variant="secondary">
+          Create a new Id
+        </Button>
       </Form>
     </Container>
   );
